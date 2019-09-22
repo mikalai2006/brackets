da748b499a3257410357b8fcf9dbd0e3fe6dd079
mikalai2006 <composer.opencart@gmail.com>
2019-09-22 21:08:41 +0300

    fix: add js code in function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  
+  let resultStack = [...str];
+  for (let i = 0; i < resultStack.length; i++) {
+      for (let j = 0; j < bracketsConfig.length; j++) {
+          if ((resultStack[i] === bracketsConfig[j][0]) && (resultStack[i + 1] === bracketsConfig[j][1])) {
+              resultStack.splice(i, 2);
+              i = i - 2;
+          };
+      }
+  }
+  return resultStack.length === 0;
+
 }
